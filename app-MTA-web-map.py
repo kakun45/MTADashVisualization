422d6ce637d3ffe27b7612f8983f5b1bb98e4691
kakun45 <email4blackkat@gmail.com>
2022-03-26 23:11:25 -0400

    formated with black main folder

--- app-MTA-web-map.py
+++ app-MTA-web-map.py
@@ -1,46 +1,67 @@
 import folium
-import pandas 
+import pandas
 
-data = pandas.read_csv('_midtown_timebins_sum_grater16_sort.csv')
-lat = list(data['LAT']) 
-lon = list(data['LON'])
-exits = list(data['EXITS_diff_sum'])
-entries = list(data['ENTRIES_diff_sum'])
-station = list(data['station_id'])
+data = pandas.read_csv("_midtown_timebins_sum_grater16_sort.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+exits = list(data["EXITS_diff_sum"])
+entries = list(data["ENTRIES_diff_sum"])
+station = list(data["station_id"])
+
 
 def color_producer_ex(exits):
     if exits < 3000:
-        return 'beige'
+        return "beige"
     elif 3000 <= exits < 10000:
-        return 'orange'
+        return "orange"
     else:
-        return 'pink'
+        return "pink"
 
-map = folium.Map(location=[40.762796, -73.967686], zoom_start=14, tiles='Stamen Toner')
 
-fgv = folium.FeatureGroup(name='MTA_Exits')
+map = folium.Map(location=[40.762796, -73.967686], zoom_start=14, tiles="Stamen Toner")
+
+fgv = folium.FeatureGroup(name="MTA_Exits")
 
 for lt, ln, ex, st in zip(lat, lon, exits, station):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = ex/500, popup=str(ex)+' ppl Exit @ '+(st), 
-    fill_color=color_producer_ex(ex), fill=False, color='red', fill_opacity=0.1))
+    fgv.add_child(
+        folium.CircleMarker(
+            location=[lt, ln],
+            radius=ex / 500,
+            popup=str(ex) + " ppl Exit @ " + (st),
+            fill_color=color_producer_ex(ex),
+            fill=False,
+            color="red",
+            fill_opacity=0.1,
+        )
+    )
 
-fgp = folium.FeatureGroup(name='MTA_Entries')
+fgp = folium.FeatureGroup(name="MTA_Entries")
 
 
 def color_producer_ent(entries):
     if entries < 3000:
-        return 'lightgreen'
+        return "lightgreen"
     elif 3000 <= entries < 10000:
-        return 'yellow'
+        return "yellow"
     else:
-        return 'darkgreen'
+        return "darkgreen"
+
 
 for lt, ln, en, st in zip(lat, lon, entries, station):
-    fgp.add_child(folium.CircleMarker(location=[lt, ln], radius = en/500, popup=str(en)+' ppl Enter @'+(st), 
-    fill_color=color_producer_ent(en), fill=False, color='green', fill_opacity=0.1))
+    fgp.add_child(
+        folium.CircleMarker(
+            location=[lt, ln],
+            radius=en / 500,
+            popup=str(en) + " ppl Enter @" + (st),
+            fill_color=color_producer_ent(en),
+            fill=False,
+            color="green",
+            fill_opacity=0.1,
+        )
+    )
 
 map.add_child(fgv)
 map.add_child(fgp)
 map.add_child(folium.LayerControl())
 
-map.save('Map5.html')
+map.save("Map5.html")
--- callback-MTA-ent_ext-Map.py
+++ callback-MTA-ent_ext-Map.py
@@ -1,4 +1,4 @@
-import dash 
+import dash
 import numpy as np
 import dash_core_components as dcc
 import dash_html_components as html
@@ -6,83 +6,116 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
 import folium
-#get help from installed module: 
-#in terminal
-#import dash_html_components as html
-#print(help(html.Div))
 
-#load a file
-df = pd.read_csv('_midtown_timebins_sum_grater16_sort.csv') # final works
+# get help from installed module:
+# in terminal
+# import dash_html_components as html
+# print(help(html.Div))
+
+# load a file
+df = pd.read_csv("_midtown_timebins_sum_grater16_sort.csv")  # final works
 
 
-#launch app 
+# launch app
 app = dash.Dash()
 
-#from https://dash.plot.ly/dash-core-components/dropdown
-#Crate a dash layout that contains a Graph component
+# from https://dash.plot.ly/dash-core-components/dropdown
+# Crate a dash layout that contains a Graph component
 hour_options = []
-for hour in df['HODBIN2'].unique():
-    hour_options.append({'label':str(hour),'value':hour})
+for hour in df["HODBIN2"].unique():
+    hour_options.append({"label": str(hour), "value": hour})
 
 
-daymap = { "Sunday": 0, "Monday": 1, "Tuesday": 2, "Wednesdays": 3, "Thursday": 4, "Friday": 5, "Saturday": 6}
-weekdays = list(df['WEEKDAY'])
+daymap = {
+    "Sunday": 0,
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesdays": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6,
+}
+weekdays = list(df["WEEKDAY"])
 
 
-app.layout = html.Div([
-    html.H1('Midtown, NYC: MTA Entries vs Exsits 10/12/2019 - 10/18/2019'),
-    dcc.Slider(id='hour-slider2', min=df['HODBIN2'].min(),
-        marks={str(time): str(time)+" o'clock" for time in df['HODBIN2'].unique()},
-        value=df['HODBIN2'].min(),
-        max = df['HODBIN2'].max(), step=None
-    ),
-    html.Iframe(id='map', srcDoc=open('Map5.html', 'r').read(), width = '100%', height='300', style={'border':'none'}),
-    dcc.Graph(id='graph'),
-    dcc.Slider(id='hour-slider', min=df['HODBIN2'].min(), max=df['HODBIN2'].max(), 
-    value=df['HODBIN2'].min(),
-    marks={str(time): str(time)+" o'clock" for time in df['HODBIN2'].unique()},
-    step=None
-    )  
-])
+app.layout = html.Div(
+    [
+        html.H1("Midtown, NYC: MTA Entries vs Exsits 10/12/2019 - 10/18/2019"),
+        dcc.Slider(
+            id="hour-slider2",
+            min=df["HODBIN2"].min(),
+            marks={
+                str(time): str(time) + " o'clock" for time in df["HODBIN2"].unique()
+            },
+            value=df["HODBIN2"].min(),
+            max=df["HODBIN2"].max(),
+            step=None,
+        ),
+        html.Iframe(
+            id="map",
+            srcDoc=open("Map5.html", "r").read(),
+            width="100%",
+            height="300",
+            style={"border": "none"},
+        ),
+        dcc.Graph(id="graph"),
+        dcc.Slider(
+            id="hour-slider",
+            min=df["HODBIN2"].min(),
+            max=df["HODBIN2"].max(),
+            value=df["HODBIN2"].min(),
+            marks={
+                str(time): str(time) + " o'clock" for time in df["HODBIN2"].unique()
+            },
+            step=None,
+        ),
+    ]
+)
 
 
-@app.callback(Output('graph', 'figure'), [Input('hour-slider', 'value')])
+@app.callback(Output("graph", "figure"), [Input("hour-slider", "value")])
 def update_figure(selected_time):
-    filtered_df = df[df['HODBIN2'] == selected_time]
+    filtered_df = df[df["HODBIN2"] == selected_time]
     traces = []
-    for station_name in filtered_df['station_id'].unique():
-        df_by_station = filtered_df[filtered_df['station_id'] == station_name]
-        traces.append(go.Scatter(
-            y = df_by_station['ENTRIES_diff_sum'],
-            x = df_by_station['WEEKDAY'],
-            text = df_by_station['HODBIN2'],
-            mode = 'lines+markers',
-            opacity=0.8,
-            marker={'size':7, 'line': {'width': 0.5, 'color': 'blue'}},
-            name=station_name + ' (ENTRIES)'
-        ))
-    for station_name in filtered_df['station_id'].unique():
-        df_by_station = filtered_df[filtered_df['station_id'] == station_name]
-        traces.append(go.Scatter( 
-            y = df_by_station['EXITS_diff_sum'],
-            x = df_by_station['WEEKDAY'],
-            text = df_by_station['HODBIN2'],
-            mode = 'lines',
-            opacity=0.6,
-            marker={'size':9, 'line': {'width': 0.5, 'color': 'red'}},
-            name=station_name + ' (EXITS)'
-        ))
-
-
+    for station_name in filtered_df["station_id"].unique():
+        df_by_station = filtered_df[filtered_df["station_id"] == station_name]
+        traces.append(
+            go.Scatter(
+                y=df_by_station["ENTRIES_diff_sum"],
+                x=df_by_station["WEEKDAY"],
+                text=df_by_station["HODBIN2"],
+                mode="lines+markers",
+                opacity=0.8,
+                marker={"size": 7, "line": {"width": 0.5, "color": "blue"}},
+                name=station_name + " (ENTRIES)",
+            )
+        )
+    for station_name in filtered_df["station_id"].unique():
+        df_by_station = filtered_df[filtered_df["station_id"] == station_name]
+        traces.append(
+            go.Scatter(
+                y=df_by_station["EXITS_diff_sum"],
+                x=df_by_station["WEEKDAY"],
+                text=df_by_station["HODBIN2"],
+                mode="lines",
+                opacity=0.6,
+                marker={"size": 9, "line": {"width": 0.5, "color": "red"}},
+                name=station_name + " (EXITS)",
+            )
+        )
 
     return {
-        'data': traces,
-        'layout': go.Layout(
-            yaxis={'type':'log', 'title':'Midtown: Number of People through Entries & Exits'},
-            xaxis={'title':'Weekday'},
-            hovermode='closest'
-        )
+        "data": traces,
+        "layout": go.Layout(
+            yaxis={
+                "type": "log",
+                "title": "Midtown: Number of People through Entries & Exits",
+            },
+            xaxis={"title": "Weekday"},
+            hovermode="closest",
+        ),
     }
 
-if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port=8080, debug=True)
+
+if __name__ == "__main__":
+    app.run_server(host="127.0.0.1", port=8080, debug=True)
